2d1fe98cd7bcf3e3db825513bbc30f3b5d59ffe0
TestJavagit <2967267350@qq.com>
2019-04-30 12:55:10 +0800

    激活码

--- src/main/java/cn/itcast/travel/dao/impl/UserDaoImpl.java
+++ src/main/java/cn/itcast/travel/dao/impl/UserDaoImpl.java
@@ -3,6 +3,7 @@ package cn.itcast.travel.dao.impl;
 import cn.itcast.travel.dao.UserDao;
 import cn.itcast.travel.domain.User;
 import cn.itcast.travel.util.JDBCUtils;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -44,5 +45,34 @@ public class UserDaoImpl implements UserDao {
                 user.getEmail()
 
                               );
+    }
+
+    /**
+     * 根据激活码查询用户对象
+     * @param code
+     * @return
+     */
+    @Override
+    public User findByCode(String code) {
+        User user=null;
+        try {
+            String sql="select*from tab_user where code= ?";
+            user=template.queryForObject(sql,new BeanPropertyRowMapper<User>(User.class),code);
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
+    /**
+     * 修改指定用户激活状态
+     * @param user
+     */
+    @Override
+    public void updateStatus(User user) {
+        String sql="update user set status = 'Y' where id = ?";
+        template.update(sql,user.getUid());
+
     }
 }
